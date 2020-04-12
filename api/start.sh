eb62b4be2337da425bb06fff3602dd923c28ec34
Nguyen Duy <nguyenducduy.it@gmail.com>
2020-04-12 13:43:40 +0700

    global gunicorn

--- api/start.sh
+++ api/start.sh
@@ -1,1 +1,1 @@
-exec /var/www/html/podcast-platform/api/myenv/bin/gunicorn -w 4 -b 0.0.0.0:5000 -k gevent wsgi:app -e FLASK_ENV=production
+exec gunicorn -w 4 -b 0.0.0.0:5000 -k gevent wsgi:app -e FLASK_ENV=production
